e72872391754f380de53c29a9ba5341763f33fba
ambivam <rajanikanth.bathula@gmail.com>
2021-07-15 20:38:39 +0000

    5 functional component with with Arrow functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import Greet from './components/Greet'
+import {Greet} from './components/Greet'
 
 class App extends Component {
  render(){
