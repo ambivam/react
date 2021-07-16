c3eb01a4e4a27c20222af16eb0959e38094e26e2
ambivam <rajanikanth.bathula@gmail.com>
2021-07-16 16:28:59 +0000

    React setState 11 using previous state function part 3

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,19 +10,29 @@ class Counter extends Component {
     }
 
     increment(){
-        this.setState({
-            count: this.state.count + 1
-        },() => {console.log('callback value',this.state.count)})
+        // this.setState({
+        //     count: this.state.count + 1
+        // },() => {console.log('callback value',this.state.count)})        
         
+
+        this.setState((prevState) => ({
+            count: prevState.count +1
+        }) )
         console.log(this.state.count)
     }
     
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+    }
 
     render() {
         return (
             <div>
                 <div>count - {this.state.count}</div>
-                <button onClick = {() => this.increment()}> Increment</button>
+                <button onClick = {() => this.incrementFive()}> Increment</button>
 
             </div>
         )
