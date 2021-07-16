250958dd573d2d5e568342d7ffd225e57a4aedd8
ambivam <rajanikanth.bathula@gmail.com>
2021-07-16 14:22:25 +0000

    React State 10

--- src/App.js
+++ src/App.js
@@ -4,26 +4,30 @@ import { Component } from 'react';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 class App extends Component {
  render(){
   return (
     <div className="App">
-      <Greet name='Ambica' likes='eating brain'>
+
+    <Message />
+
+      {/* <Greet name='Ambica' likes='eating brain'>
         <p>This is children props</p>
       </Greet>
       <Greet name='Teju' likes='eating '>
         <button>Action</button>
       </Greet>
-      <Greet name='Bhavya' likes='biscuts'/>
+      <Greet name='Bhavya' likes='biscuts'/> */}
 
       {/* Using class components */}
-      <Welcome name='Ambica' likes='eating brain'>
+      {/* <Welcome name='Ambica' likes='eating brain'>
         <p>This is children props</p>
       </Welcome> 
       <Welcome name='Teju' likes='eating ' />
       <Welcome name='Bhavya' likes='biscuts' />
-      {/* <Hello /> */}
+      <Hello /> */}
     </div>
   );
  }
