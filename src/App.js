73cd0423c00f280342f0aecb6df4f089486e6645
ambivam <rajanikanth.bathula@gmail.com>
2021-07-16 14:55:59 +0000

    React setState 11 part1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 class App extends Component {
  render(){
   return (
     <div className="App">
 
-    <Message />
+      <Counter />
+
+      {/* <Message /> */}
 
       {/* <Greet name='Ambica' likes='eating brain'>
         <p>This is children props</p>
