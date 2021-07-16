582e59db49d79198671df7eaf80d7a8f4e067031
ambivam <rajanikanth.bathula@gmail.com>
2021-07-16 06:57:53 +0000

    React JSX 9 adding properties to div

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Hello = () => { 
     //Using JSX
-    // return (
-    //     <div>
-    //         <h1> Hello ATB</h1>
-    //     </div>
+    return (
+        <div id='hello' className='dummyClass'>
+            <h1> Hello ATB</h1>
+        </div>
 
-    // )
+    )
 
     //********Without JSX*************
-    return React.createElement(
-        'div',
-        {id: 'hello',className:'dummyClass'},
-        React.createElement('h1',null,'Hello ATB')
-    )
+    // return React.createElement(
+    //     'div',
+    //     {id: 'hello',className:'dummyClass'},
+    //     React.createElement('h1',null,'Hello ATB')
+    // )
 
 }
 
