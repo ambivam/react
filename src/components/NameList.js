30337356193faf310e7bf51f4054687b61fa5703
ambivam <rajanikanth.bathula@gmail.com>
2021-07-17 10:24:48 +0000

    List rendering using components

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
-    const names = ['Bruce','Clarke','Diana']
-    const nameList = names.map(name=><h2>{name}</h2>)
+    //const names = ['Bruce', 'Clarke', 'Diana']
+    const persons = [
+        {
+            id:1,
+            name:'Bruce',
+            age:30,
+            skill: 'React'
+        },
+        {
+            id:2,
+            name:'Clarke',
+            age:25,
+            skill: 'Angular'
+        },
+        {
+            id:3,
+            name:'Diana',
+            age:28,
+            skill: 'vue'
+        }
+    ]
+    //const nameList = names.map(name => <h2>{name}</h2>)
+    const personsList = persons.map(person => (
+        <Person person={person} />
+    ))
+
     return (
-        <div>
-           {
-            //    names.map(name => <h2>{name}</h2>)
-            nameList
-           }       
-        </div>
+        <div>{personsList}</div>
     )
 }
 
