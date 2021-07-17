15a296b8b1ff9554d449da694ac2898925a09b25
ambivam <rajanikanth.bathula@gmail.com>
2021-07-17 09:05:13 +0000

    Using Element variable approach

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -6,25 +6,36 @@ class UserGreeting extends Component {
         super(props)
 
         this.state = {
-            isLoggedIn: true
+            isLoggedIn: false
         }
     }
 
 
     render() {
-        if (this.state.isLoggedIn) {
-            return (
-                <div>
-                    Welcome RajaniKanth
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    Welcome Guests
-                </div>
-            )
+
+        let message
+
+        if(this.state.isLoggedIn){
+            message = <div>Welcome Rajani Kanth</div>
+        }else{
+            message = <div>Welcome Guests</div>
         }
+
+        return <div>{message}</div>
+
+        // if (this.state.isLoggedIn) {
+        //     return (
+        //         <div>
+        //             Welcome RajaniKanth
+        //         </div>
+        //     )
+        // } else {
+        //     return (
+        //         <div>
+        //             Welcome Guests
+        //         </div>
+        //     )
+        // }
 
         // return (
         //     <div>
