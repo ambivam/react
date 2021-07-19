6f4fdffdc5e57abdb7e7fbf8eca41d982ac023a1
ambivam <rajanikanth.bathula@gmail.com>
2021-07-19 04:30:44 +0000

    Controlled Components Textarea and Select

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,13 +6,27 @@ class Form extends Component {
         super(props)
     
         this.state = {
-             username: ''
+             username: '',
+             comments: '',
+             topic: ''
         }
     }
 
     handleUsernameChange = (event) => {
         this.setState({
             username: event.target.value
+        })
+    }
+
+    handleCommentsChange = (event) =>{
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+    handleTopicChange = (event) =>{
+        this.setState({
+            topic: event.target.value
         })
     }
     
@@ -22,6 +36,20 @@ class Form extends Component {
                 <div>
                     <label>Username</label>
                     <input type='text' value={this.state.username} onChange={this.handleUsernameChange}/>
+                </div>
+
+                <div>
+                    <label>Comments</label>
+                    <textarea value={this.state.comments} onChange={this.handleCommentsChange} />
+                </div>
+
+                <div>
+                    <label>Topic</label>
+                    <select value={this.state.topic} onChange={this.handleTopicChange}>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+                    </select>
                 </div>
             </form>
         )
