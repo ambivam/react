10eb41f940dd4bcbee27d40cf029845f40a61800
ambivam <rajanikanth.bathula@gmail.com>
2021-07-22 06:53:35 +0000

    call back refs

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -5,12 +5,19 @@ class RefsDemo extends Component {
     constructor(props) {
         super(props)
         this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setCbRef = (element) =>{
+            this.cbRef = element
+        }
        
     }
     
     componentDidMount(){
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
+        if(this.cbRef){
+            this.cbRef.focus()
+        }
     }
 
     clickHandler = () => {
@@ -21,6 +28,7 @@ class RefsDemo extends Component {
         return (
             <div>
                 <input type="text" ref={this.inputRef} />
+                <input type="text" ref={this.setCbRef} />
                 <button onClick={this.clickHandler}>Click</button>
             </div>
         )
