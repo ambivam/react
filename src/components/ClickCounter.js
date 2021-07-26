885872b230c4326b30c694fb1732abdb6d4a5e9e
ambivam <rajanikanth.bathula@gmail.com>
2021-07-26 10:07:22 +0000

    Hogher Order Components Part2

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react'
+import withCounter from './withCounter'
+
 
 class ClickCounter extends Component {
     
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             count: 0
-        }
-    }
-    
-    incrementCount = () => {
-        this.setState(prevState => {
-            return {count: prevState.count + 1}
-        })
-    }
     
     render() {
-        const {count} = this.state
+        
+        const {count,incrementCount} = this.props
         return (
             <div>
-                <button onClick={this.incrementCount}>Clicker {count} Times</button>
+                <button onClick={incrementCount}> Clicked  {count} Times</button>
             </div>
         )
     }
 }
 
-export default ClickCounter
+export default withCounter(ClickCounter)
